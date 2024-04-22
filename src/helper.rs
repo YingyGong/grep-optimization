8620f07283d53f941151432ca54267462252208c
YingyGong <ygong@caltech.edu>
2024-04-22 11:14:18 -0700

    implemented boyer moore

--- src/helper.rs
+++ src/helper.rs
@@ -1,15 +1,182 @@
+use std::cmp::max;
+use std::vec;
+
 use crate::nfa::NFA;
 use crate::nfa;
 use crate::earley_parse::CFG;
 use crate::cfg;
 use crate::cfg::cfg_for_regular_expression;
 
+const ALPHABET_SIZE: usize = 256;
+
+// helper functions for boyer moore algorithm
+fn alphabet_index(c: char) -> usize {
+    let ans = c as usize;
+    assert!(ans < ALPHABET_SIZE && ans >= 0);
+    ans
+}
+
+// Return the length of the match of the substrings of S beginning at idx1 and idx2.
+fn match_length(s: &str, idx1: usize, idx2: usize) -> usize {
+    let mut idx1 = idx1;
+    let mut idx2 = idx2;
+    if idx1 == idx2 {
+        return s.len() - idx1
+    }
+    let mut count = 0;
+    while idx1 < s.len() && idx2 < s.len() && s.chars().nth(idx1).unwrap() == s.chars().nth(idx2).unwrap() {
+        count += 1;
+        idx1 += 1;
+        idx2 += 1;
+    }
+    count
+}
+
+fn preprocess(s: &str) -> Vec<i32> {
+    if s.len() == 0 {
+        return vec![]
+    }
+    if s.len() == 1 {
+        return vec![1]
+    }
+    let mut z = vec![0; s.len()];
+    z[0] = s.len() as i32;
+    z[1] = match_length(s, 0, 1) as i32;
+    for i in 2..(1 + z[1]) {
+        z[i as usize] = z[i as usize] - i + 1;
+    }
+    let mut l = 0;
+    let mut r = 0;
+    for i in 2 + z[1]..s.len() as i32 {
+        if i <= r {
+            let k: usize = (i - l) as usize;
+            let b = z[k];
+            let a = r - i + 1;
+            if b < a {
+                z[i as usize] = b;
+            }
+            else {
+                z[i as usize] = a + match_length(s, a as usize, r as usize) as i32;
+                l = i;
+                r = i + z[i as usize] - 1;
+            }
+        }
+        else {
+            z[i as usize] = match_length(s, 0, i as usize) as i32;
+            if z[i as usize] > 0 {
+                l = i;
+                r = i + z[i as usize] - 1;
+            }
+        }
+    }
+    z
+}
+
+fn bad_char_table(s: &str) -> Vec<Vec<i32>> { 
+    if s.len() == 0 {
+        return vec![vec![]; ALPHABET_SIZE]
+    }
+    let mut R = vec![vec![-1]; ALPHABET_SIZE];
+    let mut alpha = vec![-1; ALPHABET_SIZE];
+
+    for (i, c) in s.chars().enumerate() {
+        alpha[alphabet_index(c)] = i as i32;
+        for (j, &a) in alpha.iter().enumerate() {
+            R[j].push(a);
+        }
+    }
+    R
+}
+
+fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
+fn reverse_vec(s: &Vec<i32>) -> Vec<i32> {
+    s.iter().rev().map(|&x| x).collect()
+}
+
+fn good_suffix_table(s: &str) -> Vec<i32> {
+    let mut L = vec![-1; s.len()];
+    let mut N = preprocess(reverse_string(&s).as_str());
+    let N = reverse_vec(&N);
+    for j in 0..(s.len() -1) {
+        let i = s.len() - N[j] as usize;
+        if i != s.len() {
+            L[i] = j as i32;
+        }
+    }
+    L
+}
+
+fn full_shift_table(s: &str) -> Vec<i32> {
+    let n = s.len();
+    let mut f = vec![0; n];
+    let z = preprocess(s);
+    let mut longest = 0;
+
+    for (i, &zv) in z.iter().rev().enumerate() {
+        if zv == (i + 1) as i32 {
+            longest = zv.max(longest);
+        }
+        f[n - i - 1] = longest;
+    }
+    f
+}
+
+pub fn find_prefix_boyer_moore(p: &str, t: &str) -> Vec<usize> {
+    if p.is_empty() || t.is_empty() || t.len() < p.len() {
+        return Vec::new();
+    }
+
+    let mut matches: Vec<usize> = Vec::new();
+    let r = bad_char_table(p);
+    let l = good_suffix_table(p);
+    let f = full_shift_table(p);
+
+    let mut k = p.len() -1;
+    let mut previous_k = -1isize;
+
+    while k < t.len() {
+        let mut i: isize = (p.len() -1) as isize;
+        let mut h: isize = k as isize;
+
+        while i >= 0 && (h as isize) > previous_k && p.as_bytes()[i as usize] == t.as_bytes()[h as usize]{
+            i -= 1;
+            h -= 1;
+        }
+
+        if i == -1 || (h as isize) == previous_k {
+            matches.push(k + 1 - p.len());
+            k += if p.len() > 1 { p.len() - f[1] as usize } else { 1 };
+        } else {
+            let char_shift = i as isize - r[alphabet_index(t.chars().nth(h as usize).unwrap())][i as usize] as isize;
+            let suffix_shift = if i + 1 == p.len() as isize {
+                1
+            } else if l[(i + 1) as usize] == -1 {
+                p.len() - f[(i + 1) as usize] as usize
+            } else {
+                p.len() - 1 - l[(i + 1) as usize] as usize
+            };
+            let shift = char_shift.max(suffix_shift as isize);
+            previous_k = if shift >= (i + 1) as isize { k as isize } else { previous_k };
+            k += shift as usize;
+        }
+    }
+
+    matches
+}
+
+
 pub fn check_str_prefix_extraction(regex: &str, line: &str) -> Vec<String> {
     let (prefix, rest) = cfg::prefix_and_remainder_extract_after_plus(regex);
-    let mut start_positions = vec![]; // the ending position of the prefix in the line
 
     // find all the prefixes in the line
-    line.match_indices(&prefix).for_each(|(start, _)| start_positions.push(start + prefix.len()));
+    let mut start_positions = find_prefix_boyer_moore(&prefix, line);
+    // add length of prefix to the start_positions
+    for i in 0..start_positions.len() {
+        start_positions[i] += prefix.len();
+    }
 
     let mut output_strs_with_prefix = vec![];
 
@@ -167,5 +334,13 @@ mod tests {
         for output_str in output_strs {
             println!("{}", output_str);
         }
+    }
+
+    #[test]
+    fn test_boyer_moore() {
+        let p = "abab";
+        let t = "ababababab";
+        let matches = find_prefix_boyer_moore(p, t);
+        println!("matches: {:?}", matches);
     }
 }
